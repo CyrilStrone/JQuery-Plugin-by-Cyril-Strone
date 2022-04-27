490969faedad430d4d53facd1f4d60971c413ffe
Cyril Strone <kidvos@gmail.com>
2022-04-27 23:04:46 +0700

    Update
    Можно менять длину, ширину, стартовые позиции и шаг прямо в браузере из инпутов. Добавил уан клик который при запуске сам нажимается и задает начальные значения.

--- slider/my/slider-strone copy.js
+++ slider/my/slider-strone copy.js
@@ -1,36 +1,22 @@
 let SliderGeneral = document.querySelector('.slider-general');
-let MaximumValue;
-let VorG = true;
-let SliderLineWidth;
-let SliderLineHeight;
-let SliderLineMinWidth;
-let SliderLineMinHeight;
-let SliderStepSize;
+
+let VorG = false;
+let MaximumValue = 15000;
+
+let SliderLineWidth = 300;
+let SliderLineHeight = 5;
+let SliderLineMinWidth = SliderLineWidth;
+let SliderLineMinHeight = SliderLineHeight;
+let SliderStepSize = 500;
 let SliderCircleOneMarginTop;
 let SliderCircleOneMarginLeft;
 let SliderCircleTwoMarginTop;
 let SliderCircleTwoMarginLeft;
-let SliderCircleOneWidthHeight;
-let SliderCircleTwoWidthHeight;
+let SliderCircleOneWidthHeight = SliderLineHeight * 2;
+let SliderCircleTwoWidthHeight = SliderLineHeight * 2;
 let SliderCircleBorder = 2;
-let startingPointViewCircleOne;
-let startingPointViewCircleTwo;
-
-let coefficient;
-let startingPointCircleOne;
-let startingPointCircleTwo;
-let newLeftCircleOne;
-let newLeftCircleTwo;
-
-let SliderLineMargin;
-
-function fn(a, num) {
-
-    return a % num ? a + num - a % num : a
-
-};
-
-
+let startingPointViewCircleOne = 5000;
+let startingPointViewCircleTwo = 10000;
 
 
 let SliderTitleOne = document.createElement('slider-title_one')
@@ -44,6 +30,7 @@ SliderTitleOne.style.cssText = `
   line-height: 15px;
   text-transform: uppercase;
   color: #1F2041;`;
+
 let SliderMoney = document.createElement('slider-money')
 SliderMoney.style.cssText = `
   float:right;
@@ -54,12 +41,15 @@ SliderMoney.style.cssText = `
   font-size: 12px;
   line-height: 14px;
   color: rgba(31, 32, 65, 0.5);`;
+
 let SliderMoneyOne = document.createElement('slider-money-one')
 SliderMoneyOne.textContent = `₽0`;
 let SliderMoneyThree = document.createElement('slider-money-three')
 SliderMoneyOne.textContent = ` - `;
 let SliderMoneyTwo = document.createElement('slider-money-two')
 SliderMoneyTwo.textContent = `₽0`;
+
+
 let SliderLine = document.createElement('slider')
 SliderLine.id = 'slider';
 SliderLine.style.cssText = `
@@ -67,6 +57,24 @@ SliderLine.style.cssText = `
   background: #FFFFFF;
   border: 1px solid rgba(31, 32, 65, 0.25);
   border-radius: 3px;`;
+
+
+let SliderLineMargin;
+if (VorG == true) {
+    SliderLineMargin = 30;
+    SliderLine.style.width = SliderLineWidth + 'px';
+    SliderLine.style.height = SliderLineHeight + 'px';
+    SliderLine.style.marginTop = SliderLineMargin + 'px';
+} else {
+    SliderLineMargin = 20;
+    SliderLine.style.width = SliderLineHeight + 'px';
+    SliderLine.style.height = SliderLineWidth + 'px';
+    SliderLine.style.marginLeft = SliderLineMargin + 'px';
+}
+
+
+
+
 let SliderCircleOne = document.createElement('slider-circle_one')
 SliderCircleOne.id = 'CircleOne';
 SliderCircleOne.style.cssText = `
@@ -76,6 +84,20 @@ SliderCircleOne.style.cssText = `
   background: linear-gradient(180deg, #6FCF97 0%, #66D2EA 100%);
   border: 2px solid #FFFFFF;
   border-radius: 10px;`;
+
+if (VorG == true) {
+    SliderCircleOneMarginTop = -(SliderLineHeight / 2 + (SliderCircleBorder));
+    SliderCircleOne.style.marginTop = SliderCircleOneMarginTop + 'px';
+} else {
+    SliderCircleOneMarginTop = 0;
+    SliderCircleOneMarginLeft = -(SliderLineHeight / 2 + (SliderCircleBorder));
+    SliderCircleOne.style.marginTop = SliderCircleOneMarginTop + 'px';
+    SliderCircleOne.style.marginLeft = SliderCircleOneMarginLeft + 'px';
+}
+SliderCircleOne.style.width = SliderCircleOneWidthHeight + 'px';
+SliderCircleOne.style.height = SliderCircleOneWidthHeight + 'px';
+
+
 let SliderCircleTwo = document.createElement('slider-circle_two')
 SliderCircleTwo.id = 'CircleTwo';
 SliderCircleTwo.style.cssText = `
@@ -85,11 +107,34 @@ SliderCircleTwo.style.cssText = `
   background: linear-gradient(180deg, #6FCF97 0%, #66D2EA 100%);
   border: 2px solid #FFFFFF;
   border-radius: 10px;`;
+
+if (VorG == true) {
+    SliderCircleTwoMarginTop = -(SliderLineHeight / 2 + (SliderCircleBorder));
+    SliderCircleTwo.style.marginTop = SliderCircleTwoMarginTop + 'px';
+} else {
+    SliderCircleTwoMarginTop = 0;
+    SliderCircleTwoMarginLeft = -(SliderLineHeight / 2 + (SliderCircleBorder));
+    SliderCircleTwo.style.marginTop = SliderCircleTwoMarginTop + 'px';
+    SliderCircleTwo.style.marginLeft = SliderCircleTwoMarginLeft + 'px';
+}
+SliderCircleTwo.style.width = SliderCircleTwoWidthHeight + 'px';
+SliderCircleTwo.style.height = SliderCircleTwoWidthHeight + 'px';
+
 let SliderLineMin = document.createElement('slider-line_min')
 SliderLineMin.style.cssText = `
   position: relative;
   display:block;
-  background: linear-gradient(180deg, #6FCF97 0%, #66D2EA 100%);`;
+  background: linear-gradient(180deg, #6FCF97 0%, #66D2EA 100%);
+  width: 100px;
+  height: 4px;`;
+
+if (VorG == true) {
+    SliderLineMin.style.width = SliderLineMinWidth + 'px';
+    SliderLineMin.style.height = SliderLineMinHeight + 'px';
+} else {
+    SliderLineMin.style.width = SliderLineMinHeight + 'px';
+    SliderLineMin.style.height = SliderLineMinWidth + 'px';
+}
 let SliderTitleTwo = document.createElement('slider-title_two')
 SliderTitleTwo.textContent = 'Стоимость за сутки пребывания в номере';
 SliderTitleTwo.style.cssText = `
@@ -101,6 +146,9 @@ SliderTitleTwo.style.cssText = `
   font-size: 12px;
   line-height: 14px;
   color: rgba(31, 32, 65, 0.5);`;
+
+
+
 SliderGeneral.appendChild(SliderTitleOne)
 SliderGeneral.appendChild(SliderMoney)
 SliderMoney.appendChild(SliderMoneyOne)
@@ -113,118 +161,53 @@ SliderLine.appendChild(SliderLineMin)
 SliderGeneral.appendChild(SliderTitleTwo)
 
 
-var sliderbutton = document.getElementById('sliderbutton');
-sliderbutton.onclick = function(event) {
-
-    MaximumValue = document.getElementById('max').value;
-    startingPointViewCircleOne = document.getElementById('minPosCircle').value;
-    startingPointViewCircleTwo = document.getElementById('maxPosCircle').value;
-    SliderStepSize = Number(document.getElementById('stepSize').value);
-    SliderLineWidth = document.getElementById('LineWidth').value;
-    SliderLineHeight = document.getElementById('LineHeight').value;
-
-    if (MaximumValue == null || MaximumValue == '') {
-        MaximumValue = 20000;
-    }
-    if (startingPointViewCircleOne == null || startingPointViewCircleOne == '') {
-        startingPointViewCircleOne = 5000;
-    }
-    if (startingPointViewCircleTwo == null || startingPointViewCircleTwo == '') {
-        startingPointViewCircleTwo = 10000;
-    }
-    if (SliderStepSize == null || SliderStepSize == '') {
-        SliderStepSize = 50;
-    }
-    if (SliderLineWidth == null || SliderLineWidth == '') {
-        SliderLineWidth = 300;
-    }
-    if (SliderLineHeight == null || SliderLineHeight == '') {
-        SliderLineHeight = 5;
-    }
-
-    SliderLineMinWidth = SliderLineWidth;
-    SliderLineMinHeight = SliderLineHeight;
-    SliderCircleOneWidthHeight = SliderLineHeight * 2;
-    SliderCircleTwoWidthHeight = SliderLineHeight * 2;
-
-    SliderCircleOne.style.width = SliderCircleOneWidthHeight + 'px';
-    SliderCircleOne.style.height = SliderCircleOneWidthHeight + 'px';
-    SliderCircleTwo.style.width = SliderCircleTwoWidthHeight + 'px';
-    SliderCircleTwo.style.height = SliderCircleTwoWidthHeight + 'px';
-
-    coefficient = (MaximumValue / (SliderLineWidth - SliderCircleOneWidthHeight - SliderCircleBorder)).toFixed(2);
-
-    startingPointCircleOne = Math.trunc(startingPointViewCircleOne / coefficient);
-
-    startingPointCircleTwo = Math.trunc(startingPointViewCircleTwo / coefficient);
-    newLeftCircleOne = startingPointCircleOne;
-    newLeftCircleTwo = startingPointCircleTwo;
 
 
 
 
-    if (VorG == true) {
-        SliderLineMargin = 30;
-        SliderLine.style.width = SliderLineWidth + 'px';
-        SliderLine.style.height = SliderLineHeight + 'px';
-        SliderLine.style.marginTop = SliderLineMargin + 'px';
-    } else {
-        SliderLineMargin = 20;
-        SliderLine.style.width = SliderLineHeight + 'px';
-        SliderLine.style.height = SliderLineWidth + 'px';
-        SliderLine.style.marginLeft = SliderLineMargin + 'px';
-    }
-    if (VorG == true) {
-        SliderCircleOne.style.left = startingPointCircleOne + 'px';
-        SliderCircleTwo.style.left = startingPointCircleTwo + 'px';
-
-        SliderLineMin.style.left = startingPointCircleOne + 'px';
-        SliderLineMin.style.width = startingPointCircleTwo - startingPointCircleOne + 'px';
-        SliderCircleOneMarginTop = -(SliderLineHeight / 2 + (SliderCircleBorder));
-        SliderCircleOne.style.marginTop = SliderCircleOneMarginTop + 'px';
-    } else {
-        SliderCircleOne.style.top = startingPointCircleOne + 'px';
-        SliderCircleTwo.style.top = startingPointCircleTwo + 'px';
-
-        SliderLineMin.style.top = startingPointCircleOne + 'px';
-        SliderLineMin.style.height = startingPointCircleTwo - startingPointCircleOne + 'px';
-        SliderCircleOneMarginTop = 0;
-        SliderCircleOneMarginLeft = -(SliderLineHeight / 2 + (SliderCircleBorder));
-        SliderCircleOne.style.marginTop = SliderCircleOneMarginTop + 'px';
-        SliderCircleOne.style.marginLeft = SliderCircleOneMarginLeft + 'px';
-    }
-
-    if (VorG == true) {
-        SliderCircleTwoMarginTop = -(SliderLineHeight / 2 + (SliderCircleBorder));
-        SliderCircleTwo.style.marginTop = SliderCircleTwoMarginTop + 'px';
-    } else {
-        SliderCircleTwoMarginTop = 0;
-        SliderCircleTwoMarginLeft = -(SliderLineHeight / 2 + (SliderCircleBorder));
-        SliderCircleTwo.style.marginTop = SliderCircleTwoMarginTop + 'px';
-        SliderCircleTwo.style.marginLeft = SliderCircleTwoMarginLeft + 'px';
-    }
-    if (VorG == true) {
-        SliderLineMin.style.height = SliderLineMinHeight + 'px';
-    } else {
-        SliderLineMin.style.width = SliderLineMinHeight + 'px';
-    }
-
-    SliderMoneyOne.textContent = ` ₽${fn(Math.trunc(startingPointCircleOne * coefficient), SliderStepSize)}`;
-    SliderMoneyThree.textContent = ` - `;
-    SliderMoneyTwo.textContent = ` ₽${  fn(Math.trunc(startingPointCircleTwo * coefficient), SliderStepSize)}`;
 
 
-};
-sliderbutton.click();
+let coefficient = (MaximumValue / (SliderLineWidth - SliderCircleOneWidthHeight - SliderCircleBorder)).toFixed(2);
 
 
 
+let startingPointCircleOne = Math.trunc(startingPointViewCircleOne / coefficient);
+let startingPointCircleTwo = Math.trunc(startingPointViewCircleTwo / coefficient);
 
+
+let newLeftCircleOne = startingPointCircleOne;
+let newLeftCircleTwo = startingPointCircleTwo;
 
 
 if (VorG == true) {
-    SliderCircleOne.onmousedown = function(event) {
+    SliderCircleOne.style.left = startingPointCircleOne + 'px';
+    SliderCircleTwo.style.left = startingPointCircleTwo + 'px';
 
+    SliderLineMin.style.left = startingPointCircleOne + 'px';
+    SliderLineMin.style.width = startingPointCircleTwo - startingPointCircleOne + 'px';
+} else {
+    SliderCircleOne.style.top = startingPointCircleOne + 'px';
+    SliderCircleTwo.style.top = startingPointCircleTwo + 'px';
+
+    SliderLineMin.style.top = startingPointCircleOne + 'px';
+    SliderLineMin.style.height = startingPointCircleTwo - startingPointCircleOne + 'px';
+}
+
+function fn(a, num) {
+
+    return a % num ? a + num - a % num : a
+
+};
+
+
+
+
+SliderMoneyOne.textContent = ` ₽${fn(Math.trunc(startingPointCircleOne * coefficient), SliderStepSize) }`;
+SliderMoneyThree.textContent = ` - `;
+SliderMoneyTwo.textContent = ` ₽${  fn(Math.trunc(startingPointCircleTwo * coefficient), SliderStepSize)}`;
+
+if (VorG == true) {
+    SliderCircleOne.onmousedown = function(event) {
         event.preventDefault(); // предотвратить запуск выделения (действие браузера)
 
         let shiftXOne = event.clientX - SliderCircleOne.getBoundingClientRect().left;
